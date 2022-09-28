1058e71755ad1c9f9c3dad2232ff55fd218506c7
Daniel Soto <danielsoto1c@gmail.com>
2022-09-28 14:13:00 -0300

    tarea01/

--- tarea01/src/tarea01/Tarea01.java
+++ tarea01/src/tarea01/Tarea01.java
@@ -1,9 +1,56 @@
 package tarea01;
+//Obtener fecha
 
+import java.sql.Date;
+//Definicion de clases
+
+class Cliente {
+
+    public Cliente() {
+        String nombre;
+        String rut;
+    }
+    //Se supone que cada Orden de compra
+    //debe ir asociada a un cliente distinto
+    OrdenCompra asociado;
+}
+
+class OrdenCompra {
+
+    Date fecha;
+    String estado;
+
+    public int calcPrecioSinIva(int num) { 
+        return 0;
+    }
+
+    public int calcIva(int num) {
+        return 0;
+    }
+
+    public int calcPrecio(int num) {
+        return 0;
+    }
+
+    public int calcPeso() {
+        return 0;
+    }
+
+}
+class DetalleOrden {
+    private int cantidad;
+}
+class Articulo{
+    float peso;
+    String nombre;
+    String descripcion;
+    float precio;
+}
 public class Tarea01 {
 
     public static void main(String[] args) {
-        System.out.println("hola");
+        OrdenCompra m = new OrdenCompra();
+        System.out.println(m);
     }
 
 }
