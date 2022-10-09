0be94a653d4b5108ef580b32f976872956fc9112
Franchesca Mora <francmora2021@inf.udec.cl>
2022-10-09 17:49:42 -0300

    Clase DocTributario listo CREO

--- tarea01/src/tarea01/Tarea01.java
+++ tarea01/src/tarea01/Tarea01.java
@@ -72,10 +72,13 @@ class Direccion {
 }
 
 class OrdenCompra {
-
+    private Date date;
+    private String state;
+    public DetalleOrden order;
+        
     public OrdenCompra() {
-        Date fecha = new Date();
-        System.out.println(fecha);
+        order= new DetalleOrden();
+        
     }
 
     public int calcPrecioSinIva(int num) {
@@ -193,14 +196,20 @@ public abstract class Pago{
 
     private float monto;
     private Date date;
-
-    public Pago() {
+    public OrdenCompra order;
+    //Necesitamos el precio del metodo CalcPrecio de la clase OrdenCompra.
+    private float money=OrdenCompra.calPrecio();
+    
+    public Pago(float monto,Date date) {
+        this.monto=monto;
+        this.date=date;
     }
 }
 
 class Efectivo extends Pago {
 
     public Efectivo() {
+        
 
     }
 
@@ -271,16 +280,60 @@ class DocTributario {
     private Date date;
     private Direccion address;
     private OrdenCompra order;
+    
+    public DocTributario(String number,String id,Date date,Direccion address,OrdenCompra order){
+        this.number=number;
+        this.id=id;
+        this.date=date;
+        this.address=address;
+        this.order=order;
+        
+    }
+    //Creamos los getter y setter necesarios por cada informacion segun UML que debe llevar.
+    public void setDatosCliente(String number,String id){
+        this.number=number;
+        this.id=id;
+    }
+    public String getDatosCliente(){
+        return number+id;
+    }
+    public void setDate(Date date){
+        this.date=date;
+    }
+    public Date getDate(){
+        return date;
+    }
+    public void setAddress(Direccion address){
+        this.address=address;
+    }
+    public Direccion getAddress(){
+        return address;
+    }
+    public void setOrder(OrdenCompra order){
+        this.order=order;
+    }
+    public OrdenCompra getOrder(){
+        return order;
+    }
+    
+    //Unimos toda la informacion con toString.
+    @Override
+    public String toString(){
+        return "-Información cliente- Número"+number+"\n R.U.T"+id+"\n Fecha: "+date+"\n Dirección: "+address;
+        
+    }
 }
 
 class Factura extends DocTributario {
-     public Factura() {
+     public Factura(String number, String id, Date date, Direccion address, OrdenCompra order) {
+        super(number, id, date, address,order);
     }
 
 }
 
 class Boleta extends DocTributario{
-     public Boleta() {
+    public Boleta(String number, String id, Date date, Direccion address, OrdenCompra order) {
+        super(number, id, date, address,order);
     }
 }
 
